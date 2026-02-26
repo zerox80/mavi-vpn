fcc0d322cdcdd17a0836fb2f1b2ac4980de46c55
zerox80 <rujbin@proton.me>
2026-02-26 11:47:24 +0100

    Remove Kotlin/Compose plugins (built-in with AGP 9.0)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
 
 buildscript {
